43dda999c78a51d3e99f58d5363b831b19483f84
Kishor6203 <kishornd786@gmail.com>
2025-04-12 00:10:53 +0530

    Update Mainclass5.java

--- arraylist/Mainclass5.java
+++ arraylist/Mainclass5.java
@@ -5,11 +5,11 @@ import java.util.ArrayList;
 public class Mainclass5 {
 	public static void main(String[] args) {
 		System.out.println("main method started");
-		Employee emp1=new Employee(101,"Ramesh",21000.00,1.2);
-		Employee emp2=new Employee(102,"Kishor",25000.00,1.5);
-		Employee emp3=new Employee(103,"Divya",20000.00,2.0);
-		Employee emp4=new Employee(104,"Anamika",10000.00,1.1);
-		Employee emp5=new Employee(105,"Arpita",30000.00,2.5);
+		Employee emp1=new Employee(101,"Bheem",21000.00,1.2);
+		Employee emp2=new Employee(102,"Chutki",25000.00,1.5);
+		Employee emp3=new Employee(103,"Raju",20000.00,2.0);
+		Employee emp4=new Employee(104,"Indumati",10000.00,1.1);
+		Employee emp5=new Employee(105,"Kalia",30000.00,2.5);
 		
 		ArrayList<Employee>empList=new ArrayList<Employee>();
 		
