47a3bac867b9db67255e970d25692a68ca1b73da
Kishor6203 <kishornd786@gmail.com>
2025-04-12 00:05:21 +0530

    Update Mainclass1.java

--- set/Mainclass1.java
+++ set/Mainclass1.java
@@ -5,12 +5,12 @@ import java.util.TreeSet;
 public class Mainclass1 {
 	public static void main(String[] args) {
 		TreeSet<Employee> empSet=new TreeSet<Employee>();
-		Employee e1=new Employee(1011, "Kishor",35000.00);
-		Employee e2=new Employee(1102, "Abhi",25000.00);
-		Employee e3=new Employee(2065, "Abhinav",30000.00);
-		Employee e4=new Employee(1001, "Manisha",27000.00);
-		Employee e5=new Employee(2150, "Puja",15000.00);
-		Employee e6=new Employee(1102, "Susmita",45000.00);
+		Employee e1=new Employee(1011, "Bheem",35000.00);
+		Employee e2=new Employee(1102, "Chutki",25000.00);
+		Employee e3=new Employee(2065, "Raju",30000.00);
+		Employee e4=new Employee(1001, "Indumati",27000.00);
+		Employee e5=new Employee(2150, "Jaggu",15000.00);
+		Employee e6=new Employee(1102, "Kalia",45000.00);
 		
 		empSet.add(e1);
 		empSet.add(e2);
